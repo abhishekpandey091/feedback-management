f2d772e3c38110b7ce4f6d049a89107164c82583
abhishekpandey091 <pandeyabhishek09120@gmail.com>
2026-07-28 22:43:59 +0530

    Add student validation and form auto deactivation

--- server/seedAdmin.js
+++ server/seedAdmin.js
@@ -1,26 +1,47 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("./models/User");
 
-async function updateAdminPassword() {
+async function seedAdmin() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/feedback_app");
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected");
 
     const hashedPassword = await bcrypt.hash("admin123", 10);
 
-    await User.updateOne(
-      { email: "admin@example.com" },
-      { password: hashedPassword },
-    );
+    const existingAdmin = await User.findOne({
+      email: "admin@example.com",
+    });
 
-    console.log("Admin password hashed successfully");
+    if (existingAdmin) {
+      existingAdmin.password = hashedPassword;
+      existingAdmin.role = "admin";
+      existingAdmin.isActive = true;
+
+      await existingAdmin.save();
+
+      console.log("Existing admin updated");
+    } else {
+      await User.create({
+        fullName: "Admin",
+        email: "admin@example.com",
+        password: hashedPassword,
+        role: "admin",
+        isActive: true,
+      });
+
+      console.log("Admin created successfully");
+    }
 
     await mongoose.connection.close();
+    console.log("MongoDB connection closed");
   } catch (error) {
-    console.log("Error:", error);
+    console.error("Error:", error);
+    await mongoose.connection.close();
   }
 }
 
-updateAdminPassword();
+seedAdmin();
